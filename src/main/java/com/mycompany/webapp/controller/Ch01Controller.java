d6c781ea0616faec782052a61a0828b74e3fde48
akana0321 <akana0321@gmail.com>
2022-04-11 09:55:08 +0900

    log4j 1.2 -> log4j 2.17

--- src/main/java/com/mycompany/webapp/controller/Ch01Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch01Controller.java
@@ -6,12 +6,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/ch01")	// 공통경로 설정
 public class Ch01Controller {
 	private static final Logger logger = LoggerFactory.getLogger(Ch01Controller.class);
 	
 	// @RequestMapping(요청메시지): 해당 요청 메시지가 들어올 경우 아래의 메소드 실행
-	@RequestMapping("/ch01/content")
+	@RequestMapping("/content")
 	public String content() {
+		logger.info("실행");
+		return "ch01/content";
+	}
+	
+	@RequestMapping("/content2")
+	public String content2() {
+		logger.info("실행");
+		return "ch01/content";
+	}
+	
+	@RequestMapping("/content3")
+	public String content3() {
 		logger.info("실행");
 		return "ch01/content";
 	}
