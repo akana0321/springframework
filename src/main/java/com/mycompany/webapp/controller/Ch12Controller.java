da80bd85c76010983d82198843266cbbff3507ac
akana0321 <akana0321@gmail.com>
2022-04-18 17:18:45 +0900

    Ch12. View/BeanNameViewResolver

---
+++ src/main/java/com/mycompany/webapp/controller/Ch12Controller.java
@@ -0,0 +1,32 @@
+package com.mycompany.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/ch12")
+@Log4j2
+public class Ch12Controller {
+	@RequestMapping("/content")
+	public String content() {
+		log.info("실행");
+		return "ch12/content";
+	}
+	
+	@RequestMapping("/fileList")
+	public String fileList() {
+		log.info("실행");
+		return "ch12FileListView";	// 첫자를 소문자로 한 이름으로 관리
+	}
+	
+	@RequestMapping("/fileDownload")
+	public String fileDownload(@ModelAttribute("fileName") String fileName,
+							   @ModelAttribute("userAgent") @RequestHeader("User-Agent") String userAgent) {
+		log.info("실행");
+		return "ch12FileDownloadView";
+	}
+}
