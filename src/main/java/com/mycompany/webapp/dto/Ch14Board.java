ce7e926a70a3078c717578d74cfdfd7d590e007f
akana0321 <akana0321@gmail.com>
2022-04-21 09:17:21 +0900

    Ch14-2 Data Access Object - Board Example

--- src/main/java/com/mycompany/webapp/dto/Ch14Board.java
+++ src/main/java/com/mycompany/webapp/dto/Ch14Board.java
@@ -9,11 +9,12 @@ public class Ch14Board {
 	private int bno;
 	private String btitle;
 	private String bcontent;
-	private Date bdate;
+	private Date bdate;				// 기본값
 	private String mid;
+	/* battach로 아래 4개를 얻음 */
 	private String bhitcount;
 	private String battachoname;
 	private String battachsname;
 	private String battachtype;
-	private MultipartFile battach;
+	private MultipartFile battach;	// 실제 넘어오는 파일
 }
